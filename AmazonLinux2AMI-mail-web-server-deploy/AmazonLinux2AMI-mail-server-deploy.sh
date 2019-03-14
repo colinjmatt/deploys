523183f45ac6226ca310be088f5eae4ff420c738
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-03-14 20:17:37 +0000

    Revert "Revert "Revert "Remove deprecated option"""
    This reverts commit 4bcd0b0c8570f24f6a693a71c27959f597bf484b.

--- AmazonLinux2AMI-mail-web-server-deploy/AmazonLinux2AMI-mail-server-deploy.sh
+++ AmazonLinux2AMI-mail-web-server-deploy/AmazonLinux2AMI-mail-server-deploy.sh
@@ -70,7 +70,6 @@ cat ./Configs/dovecot-sieve >/etc/skel/.dovecot-sieve
 # Optimise motd
 update-motd --disable
 cat ./Configs/motd >/etc/motd
-sed -i -e "s/\$DOMAIN/""$DOMAIN""/g"
 
 # Configure dnsmasq
 cat ./Configs/dnsmasq.conf >/etc/dnsmasq.conf
