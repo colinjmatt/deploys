2f058f2be2c59f945db09ffe0ebd029719111ff3
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-04-01 20:13:48 +0100

    No more sudo

--- AL2-bookstack-docker/AL2-bookstack-docker.sh
+++ AL2-bookstack-docker/AL2-bookstack-docker.sh
@@ -9,28 +9,28 @@ if [ -f /tmp/bookstack-install ]; then
 else
   touch /tmp/bookstack-install
 	# Install required packages
-	sudo yum install docker git
+	yum install docker git -y
 
 	nginx=$(amazon-linux-extras list | grep nginx | awk -F ' ' '{print $2}')
-	sudo amazon-linux-extras install "$nginx" -y
+	amazon-linux-extras install "$nginx" -y
 
 	cd /tmp || exit
 	curl -O http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	sudo yum install epel-release-latest-7.noarch.rpm
-	sudo yum install certbot
+	yum install epel-release-latest-7.noarch.rpm
+	yum install certbot
 
 	# Enable and start docker and add user to group
-	sudo systemctl enable docker --now
-	sudo usermod -aG docker ec2-user # Logout required after this step
+	systemctl enable docker --now
+	usermod -aG docker ec2-user # Logout required after this step
 	echo "This script will now log you out. Log back in as $USER and run this script again to continue"
 	sleep 10
 	exit
 fi
 
 # Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Install Bookstack docker image
 cd ~ || exit
@@ -40,23 +40,23 @@ git clone https://github.com/solidnerd/docker-bookstack.git
 sed -i -e "s/image\:\ solidnerd\/bookstack\:.*/image\: solidnerd\/bookstack\:latest/g" docker-compose.yml
 
 # Create docker-bookstack service and enable it
-sudo cp ./Configs/docker-bookstack.service /etc/systemd/system/docker-bookstack.service
-sudo systemctl enable docker-bookstack --now
+cp ./Configs/docker-bookstack.service /etc/systemd/system/docker-bookstack.service
+systemctl enable docker-bookstack --now
 
 # Configure nginx pre certbot
-sudo mkdir -p /var/www/docker-bookstack/.well-known
-sudo mkdir -p /etc/nginx/sites
-sudo cp ./Configs/nginx.conf /etc/nginx/nginx.conf
-sudo cp ./Configs/docker-bookstack-pre-certbot.conf /etc/nginx/sites/docker-bookstack.conf
-sudo sed -i -e "s/\$domain/""$domain""/g" /etc/nginx/sites/docker-bookstack.conf
+mkdir -p /var/www/docker-bookstack/.well-known
+mkdir -p /etc/nginx/sites
+cp ./Configs/nginx.conf /etc/nginx/nginx.conf
+cp ./Configs/docker-bookstack-pre-certbot.conf /etc/nginx/sites/docker-bookstack.conf
+sed -i -e "s/\$domain/""$domain""/g" /etc/nginx/sites/docker-bookstack.conf
 
 # Start nginx
-sudo systemctl enable nginx --now
+systemctl enable nginx --now
 
 # Configure certbot
-sudo certbot certonly --agree-tos --register-unsafely-without-email --webroot -w /var/www/docker-bookstack -d "$domain"
+certbot certonly --agree-tos --register-unsafely-without-email --webroot -w /var/www/docker-bookstack -d "$domain"
 
 # Configure nginx for SSL with port 80 redirect and reload
-sudo cp ./Configs/docker-bookstack-post-certbot.conf /etc/nginx/sites/docker-bookstack.conf
-sudo sed -i -e "s/\$domain/""$domain""/g" /etc/nginx/sites/docker-bookstack.conf
-sudo systemctl reload nginx
+cp ./Configs/docker-bookstack-post-certbot.conf /etc/nginx/sites/docker-bookstack.conf
+sed -i -e "s/\$domain/""$domain""/g" /etc/nginx/sites/docker-bookstack.conf
+systemctl reload nginx
