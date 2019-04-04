d38cd22d0051e59ec0fcc921fae96521c4390e12
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-04-04 20:31:36 +0100

    Initial commit

---
+++ Download/plex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Install plex (very basic but here for reference)
+
+( cd /tmp || return
+wget https://downloads.plex.tv/plex-media-server-new/1.15.2.793-782228f99/redhat/plexmediaserver-1.15.2.793-782228f99.x86_64.rpm
+yum install plexmediaserver-1.15.2.793-782228f99.x86_64.rpm  -y )
+bash -c "$(wget -qO - https://raw.githubusercontent.com/mrworf/plexupdate/master/extras/installer.sh)"
+
+firewall-cmd --permanent --zone=drop --add-port="32400"/tcp
