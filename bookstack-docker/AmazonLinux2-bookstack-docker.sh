538d292fc5184b4bbb4f95949b78b8bb52cc8f0c
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-04-08 11:33:29 +0100

    User can now be specified

--- bookstack-docker/AmazonLinux2-bookstack-docker.sh
+++ bookstack-docker/AmazonLinux2-bookstack-docker.sh
@@ -2,30 +2,22 @@
 # Bookstack docker deployment using Amazon Linux 2
 # Set the domain name to be used BEFORE running this script
 domain="example.com"
+# Name of the user that the docker container will be run as (user must exist)
+dockeruser="ec2-user"
 
-# Check if this block has already been done
-if [ -f /tmp/bookstack-install ]; then
-    rm /tmp/bookstack-install
-else
-  touch /tmp/bookstack-install
-	# Install required packages
-	yum install docker git -y
+# Install required packages
+yum install docker git -y
+nginx=$(amazon-linux-extras list | grep nginx | awk -F ' ' '{print $2}')
+amazon-linux-extras install "$nginx" -y
 
-	nginx=$(amazon-linux-extras list | grep nginx | awk -F ' ' '{print $2}')
-	amazon-linux-extras install "$nginx" -y
+( cd /tmp || return
+curl -O http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install epel-release-latest-7.noarch.rpm )
+yum install certbot
 
-	cd /tmp || exit
-	curl -O http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	yum install epel-release-latest-7.noarch.rpm
-	yum install certbot
-
-	# Enable and start docker and add user to group
-	systemctl enable docker --now
-	usermod -aG docker ec2-user # Logout required after this step
-	echo "This script will now log you out. Log back in as $USER and run this script again to continue"
-	sleep 10
-	exit
-fi
+# Enable and start docker and add user to group
+systemctl enable docker --now
+usermod -aG docker "$dockeruser" # Logout required after this step
 
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -33,11 +25,11 @@ chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Install Bookstack docker image
-cd ~ || exit
-git clone https://github.com/solidnerd/docker-bookstack.git
+( cd "$dockeruser" || exit
+git clone https://github.com/solidnerd/docker-bookstack.git )
 
-# Edit /home/ec2-user/docker-bookstack/docker-compose.yml to use latest tag
-sed -i -e "s/image\:\ solidnerd\/bookstack\:.*/image\: solidnerd\/bookstack\:latest/g" docker-compose.yml
+# Edit /home/"$dockeruser"/docker-bookstack/docker-compose.yml to use latest tag
+sed -i -e "s/image\:\ solidnerd\/bookstack\:.*/image\: solidnerd\/bookstack\:latest/g" /home/"$dockeruser"/docker-bookstack/docker-compose.yml/docker-compose.yml
 
 # Create docker-bookstack service and enable it
 cp ./Configs/docker-bookstack.service /etc/systemd/system/docker-bookstack.service
