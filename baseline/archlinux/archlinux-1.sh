19d20ea245e7e74c0036b5972de67d6467e1b463
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-04-11 13:09:08 +0100

    Rearranged hierarchy

---
+++ baseline/archlinux/archlinux-1.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Set time & keyboard map
+timedatectl set-ntp true
+loadkeys uk.map.gz
+
+# Sort the mirrorlist so downloads are fast enough
+grep --no-group-separator -A1 "United Kingdom" /etc/pacman.d/mirrorlist > /mirrorlist
+cat /mirrorlist > /etc/pacman.d/mirrorlist
+
+# Create partitions, create and open encrypted volume and mount all partitions
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sda mkpart ESP fat32 2048s 512MiB
+parted -s /dev/sda set 1 boot on
+parted -s /dev/sda mkpart primary ext4 512MiB 100%
+mkfs.vfat -F32 /dev/sda1
+
+cryptsetup luksFormat /dev/sda2
+cryptsetup luksOpen /dev/sda2 sda2-crypt
+
+pvcreate /dev/mapper/sda2-crypt
+vgcreate vg0 /dev/mapper/sda2-crypt
+lvcreate -L 2G vg0 -n swap
+lvcreate -l 100%FREE vg0 -n root
+mkswap /dev/mapper/vg0-swap
+mkfs.ext4 /dev/mapper/vg0-root
+
+mount /dev/mapper/vg0-root /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+swapon /dev/mapper/vg0-swap
+
+# Install base system
+pacstrap /mnt base base-devel openssh wget git
+
+# Copy mirrorlist to installed base system
+cp /mirrorlist /mnt/etc/pacman.d/
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+# Set /tmp as temp drive
+echo "tmpfs	/tmp tmpfs defaults,noatime,mode=1777 0 0" >> /mnt/etc/fstab
+
+# Chroot in
+arch-chroot /mnt
+
+# Unmount and reboot into installed system
+umount -R /mnt
+swapoff -a
+reboot
---
+++ baseline/archlinux/archlinux-2.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+hostname="" # single
+users="user1 user2" # multiple
+sshusers="user1 user2" # multiple
+domain="localdomain" # single
+ipaddress="0.0.0.0/0" # single
+dns="'0.0.0.0' '0.0.0.0'" # single quoted multiples
+gateway="0.0.0.0" # single
+
+# Set region and locale
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
+echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+export LANG=en_GB.UTF-8
+echo "KEYMAP=uk" > /etc/vconsole.conf
+
+# Create dhcp ethernet connection
+cat ./Configs/ethernet-static >/etc/netctl/ethernet-static
+sed -i -e "\
+    s/\$interface/""$(ls /sys/class/net/ | grep "^en")""/g
+    s/\$ipaddress/""$ipaddress""/g; \
+    s/\$gateway/""$gateway""/g; \
+    s/\$dns/""$dns""/g; \
+    s/\$domain/""$domain""/g" \
+/etc/netctl/ethernet-static
+
+# Set hostname
+hostname $hostname
+echo "$hostname" > /etc/hostname
+echo "127.0.0.1 localhost.localdomain localhost $hostname" > /etc/hosts
+
+# Configure pacman
+cat ./Configs/pacman.conf >/etc/pacman.conf
+
+# Set .bashrc  and .nanorc for users & root
+cat ./Configs/root_bashrc >/root/.bashrc
+cat ./Configs/user_bashrc >/etc/skel/.bashrc
+cat ./Configs/root_nanorc >/root/.nanorc
+cat ./Configs/user_nanorc >/etc/skel/.nanorc
+cat ./Configs/nanorc > /etc/nanorc
+
+# Set root password, create user, add to sudoers and set password
+passwd root
+
+for name in $users ; do
+    groupadd $name
+    useradd -m -g $name -G users,wheel,storage,power $name
+    passwd $name
+done
+
+for name in $sudoers ; do
+    echo "$name ALL=(ALL) ALL" > /etc/sudoers.d/$name
+    chmod 0400 /etc/sudoers.d/$name
+done
+
+# Add modules and hooks to mkinitcpio and generate
+sed -i "s/MODULES=.*/MODULES=(nls_cp437 vfat)/g" /etc/mkinitcpio.conf
+sed -i "s/HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard)/g" /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# Setup bootctl
+bootctl install
+mkdir -p /etc/pacman.d/hooks
+cat ./Configs/100-systemd-boot.hook >/etc/pacman.d/hooks/100-systemd-boot.hook
+cat ./Configs/loader.conf >/boot/loader/loader.conf
+cat ./Configs/arch.conf >/boot/loader/entries/arch.conf
+luksencryptuuid=$(blkid | grep crypto_LUKS | awk -F '"' '{print $2}')
+sed -i -e "s/\$luksencryptuuid/""$luksencryptuuid""/g" /boot/loader/entries/arch*.conf
+
+# Config for vfio reservation, blacklist nVidia driver and quiet kernel
+echo "kernel.printk = 3 3 3 3" > /etc/sysctl.d/20-quiet-printk.conf
+
+# Setup fsck after kernel load due to being removed for quiet boot
+cat ./Configs/systemd-fsck-root.service >/etc/systemd/system/systemd-fsck-root.service
+cat ./Configs/systemd-fsck\@.service >/etc/systemd/system/systemd-fsck\@.service
+
+# Configure SSH
+cat ./Configs/sshd_config >/etc/ssh/sshd_config
+sed -i -e "s/\$sshusers/""$sshusers""/g" /etc/ssh/sshd_config
+
+# Set locale
+localectl set-keymap uk
+localectl set-locale LANG="en_GB.UTF-8"
+
+# Set time synchronisation
+timedatectl set-ntp true
+
+# Enable and start networking to download more packages
+systemctl enable netctl
+netctl enable ethernet-dhcp
+
+# Enable ssh to assist with rest of setup
+systemctl enable sshd
+
+# Exit chroot
+exit
