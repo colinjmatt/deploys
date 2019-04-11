f3a091ac5d136774d3f351d85d292d222b57964c
Colin Matthews <colin@matthews-co.uk>
2019-04-12 00:50:28 +0100

    DNS config complete

--- dns/ArchLinux-dns.sh
+++ dns/ArchLinux-dns.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
-# DNS & nfs server deployment using Arch Linux
+# DNS & NFS server deployment using Arch Linux
+domain="localdomain" # Local domain name
+ipaddress="0.0.0.0" # IP address dns requests will be sent to
+dns="0.0.0.1 0.0.0.2" # List of nameservers to be used
+
+# Install packages
 pacman -S --noconfirm dnsmasq
-# /etc/dnsmasq.conf config here
-mkdir -p /etc/dnsmasq.d
-# blacklist.conf for ad blocking from pgl.yoyo.org
-# pixelserv running on 127.0.0.1 port 80
-# local.conf for local addresses
+
+# Configure dnsmasq
+cat ./Configs/dnsmasq.conf >/etc/dnsmasq.conf
+
+for ip in $dns; do
+  echo "server=$ip" >>/etc/dnsmasq.conf
+done
+
+cat ./Configs/dnsmasq.hosts >/etc/dnsmasq.hosts
+cat ./Configs/adblock.sh >/usr/local/bin/adblock.sh
+cat ./Configs/adblock.service >/etc/systemd/system/adblock.service
+cat ./Configs/adblock.timer >/etc/systemd/system/adblock.timer
+
+# Install and configure pixelserv
+wget -O /usr/local/bin/pixelserv.pl http://proxytunnel.sourceforge.net/files/pixelserv.pl.txt
+chmod +x /usr/local/bin/pixelserv.pl
+cat ./Configs/pixelserv.service >/etc/systemd/system/pixelserv.service
 
 #nfs server config here
+
+systemctl enable  adblock.timer \
+                  dnsmasq \
+                  pixelserv --now
