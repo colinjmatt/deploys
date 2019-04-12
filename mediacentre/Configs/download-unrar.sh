b970f497599685b1addbbe610b1e5c4127893234
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-04-12 11:59:13 +0100

    Simplified

--- mediacentre/Configs/download-unrar.sh
+++ mediacentre/Configs/download-unrar.sh
@@ -3,7 +3,5 @@ torrentdir="$TR_TORRENT_DIR"/"$TR_TORRENT_NAME"
 cd "$torrentdir" || return
 for archive in ./*.rar
 do
-  if [[ -e "$archive" ]]; then
-    find ./*.rar -exec /usr/local/bin/unrar e -r -o- {} \;
-  fi
+    /usr/local/bin/unrar e -r -o- "$archive"
 done
