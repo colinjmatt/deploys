3fcf90469684fa27015bfd1357d848d9bf7518e0
Colin Matthews <colin@matthews-co.uk>
2019-05-01 20:49:47 +0100

    Can now set drive letters and swap size variables

--- baseline/archlinux/archlinux-1.sh
+++ baseline/archlinux/archlinux-1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Setup a non-encrypted base install of Arch
+swapsize="16896" # Size of required swap in MiB + 512
+installdrive="sda" # Drive to install Arch on (e.g. "$installdrive")
+
 
 # Set time & keyboard map
 timedatectl set-ntp true
@@ -9,21 +12,24 @@ loadkeys uk.map.gz
 grep --no-group-separator -A1 "United Kingdom" /etc/pacman.d/mirrorlist > /mirrorlist
 cat /mirrorlist > /etc/pacman.d/mirrorlist
 
+# Install git (if this script has been copy/pasted)
+pacman -Sy && pacman -S git --noconfirm
+
 # Create partitions, create and open encrypted volume and mount all partitions
-parted -s /dev/sda mklabel gpt
-parted -s /dev/sda mkpart ESP fat32 2048s 512MiB
-parted -s /dev/sda set 1 boot on
-parted -s /dev/sda mkpart primary ext4 512MiB 2560MiB
-parted -s /dev/sda mkpart primary ext4 2560MiB 100%
+parted -s /dev/"$installdrive" mklabel gpt
+parted -s /dev/"$installdrive" mkpart ESP fat32 2048s 512MiB
+parted -s /dev/"$installdrive" set 1 boot on
+parted -s /dev/"$installdrive" mkpart primary ext4 512MiB "$swapsize"MiB
+parted -s /dev/"$installdrive" mkpart primary ext4 "$swapsize"MiB 100%
 
-mkfs.vfat -F32 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
+mkfs.vfat -F32 /dev/"$installdrive"1
+mkswap /dev/"$installdrive"2
+mkfs.ext4 /dev/"$installdrive"3
 
-mount /dev/sda3 /mnt
+mount /dev/"$installdrive"3 /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-swapon /dev/sda2
+mount /dev/"$installdrive"1 /mnt/boot
+swapon /dev/"$installdrive"2
 
 # Install base system
 pacstrap /mnt base base-devel openssh wget git haveged
@@ -35,8 +41,15 @@ genfstab -pU /mnt >> /mnt/etc/fstab
 # Set /tmp as temp drive
 echo "tmpfs	/tmp tmpfs defaults,noatime,mode=1777 0 0" >> /mnt/etc/fstab
 
-# Chroot in
-arch-chroot /mnt
+# Clone the deploy repo to the new install and chroot in to run next script
+cat ./Configs/strap.sh >/mnt/strap.sh
+(cd /mnt || return
+git clone https://github.com/colinjmatt/deploys)
+sed -i -e "s/installdrive=\"\"/installdrive=installdrive=\"""$installdrive=""\"/g" /mnt/deploys/baseline/archlinux/archlinux-2.sh
+nano /mnt/deploys/baseline/archlinux/archlinux-2.sh
+chmod +x /mnt/deploys/baseline/archlinux/archlinux-2.sh /mnt/strap.sh
+
+arch-chroot /mnt ./strap.sh
 
 # Unmount and reboot into installed system
 umount -R /mnt
