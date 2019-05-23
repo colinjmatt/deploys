02a3dc07bf0ebb6335b0af93c5a00a38c551df6e
Colin Matthews <colin@matthews-co.uk>
2019-05-23 20:05:35 +0100

    Added command to SSH tunnel

--- mediacentre/plex.sh
+++ mediacentre/plex.sh
@@ -7,3 +7,6 @@ yum install plexmediaserver-1.15.2.793-782228f99.x86_64.rpm  -y )
 bash -c "$(wget -qO - https://raw.githubusercontent.com/mrworf/plexupdate/master/extras/installer.sh)"
 
 firewall-cmd --permanent --zone=drop --add-port="32400"/tcp
+
+# Command to ssh tunnel to sign into plex
+ssh -L 32400:localhost:32400 $domain
