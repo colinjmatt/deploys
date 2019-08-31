fee0725171dfe05b16723a296b88887b85bf22f3
Colin Matthews <colin@matthews-co.uk>
2019-08-31 17:30:08 +0100

    Update ArchLinux-dns.sh

--- dns/ArchLinux-dns.sh
+++ dns/ArchLinux-dns.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # DNS server deployment using Arch Linux
 domain="localdomain" # Local domain name
-ipaddress="0.0.0.0" # IP address dns requests will be sent to
+ipaddress="0.0.0.0" # IP address dns requests will be sent to. comma separated if multiple
 dns="0.0.0.1 0.0.0.2" # List of nameservers to be used
-
+netprofile="network-profile" # change to the netctl profile currently in use
 # Install packages
 pacman -S --noconfirm dnsmasq dnsutils
 
@@ -16,11 +16,15 @@ cat ./Configs/dnsmasq.conf >/etc/dnsmasq.conf
 for ip in $dns; do
   echo "server=$ip" >>/etc/dnsmasq.conf
 done
-sed -i -e "s/DNS=.*/DNS=('127.0.0.1')/g" /etc/netctl/ethernet-static # change to the netctl profile currently in use
+sed -i -e "s/DNS=.*/DNS=('127.0.0.1')/g" /etc/netctl/"$netprofile"
+
+sed -i -e " s/\$domain/""$domain""/g
+            s/\$ipaddress/""$ipaddress""/g" \
+            /etc/dnsmasq.conf
 
 # Install and configure pixelserv
 wget -O /usr/local/bin/pixelserv.pl http://proxytunnel.sourceforge.net/files/pixelserv.pl.txt
-chmod +x /usr/local/bin/pixelserv.pl
+chmod +x /usr/local/bin/pixelserv.pl /usr/local/bin/adblock.sh
 cat ./Configs/pixelserv.service >/etc/systemd/system/pixelserv.service
 
 # Enable services
