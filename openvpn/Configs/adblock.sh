91db61de238d10b7598eb6dc46a72ea5ea9be274
Colin Matthews <colin@matthews-co.uk>
2019-09-03 16:31:07 +0100

    Pixelserv now sends pixels to clients

--- openvpn/Configs/adblock.sh
+++ openvpn/Configs/adblock.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget -O /etc/dnsmasq.adblock 'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro=0&mimetype=plaintext'
+sed -i -e "s/127.0.0.1/10.8.0.1/g" /etc/dnsmasq.adblock
 systemctl restart dnsmasq
