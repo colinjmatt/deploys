00fca3070293ef88432a088af7bf14cb45da5faa
Colin Matthews <colin@matthews-co.uk>
2020-06-09 17:35:42 +0100

    Update download-cleanup.sh
    Now handles whitespace in file and folder names

--- mediacentre/Configs/download-cleanup.sh
+++ mediacentre/Configs/download-cleanup.sh
@@ -2,21 +2,27 @@
 transmissionuser=''
 transmissionpass=''
 email=''
+from=''
+IFS='/'
+
 # Total counted downloads
-downloadstotal=$(ls /Media/Downloads/Complete)
+downloadstotal=$(cd /Media/Downloads/Complete || exit; find -- * -maxdepth 0 | sed "s/^/'/;s/$/'/")
 
 # Acive downloads counted
-downloadsactive=$(transmission-remote -n "$transmissionuser":"$transmissionpass" -l | awk -F ' ' ' {print $10}')
+downloadsactive=$(transmission-remote -n "$transmissionuser":"$transmissionpass" -l | awk -F 'Idle         ' ' {print $2}' | tail -n +2 | head -n -1 | sed "s/^/'/;s/$/'/")
 
 # Any counted downloads that aren't active and can therefore be deleted
-downloadstodelete=$(echo "$downloadstotal" | grep -Fxv "$downloadsactive")
+downloadstodelete=$(echo "$downloadstotal" | grep -Fxv "$downloadsactive" | tr '\n' ' ')
 
-# Check that the active downloads returned are present in the total downloads counted otherwise there's something wrong
-if [ -z "$(echo "$downloadsactive" | grep -Fxvf "$downloadstotal")" ]; then
-  for download in $downloadstodelete; do
-    rm -rf "/Media/Downloads/Complete/$download"
-  done
+# Check that the active downloads returned are present in the total downloads counted otherwise there's something wrong with the data
+if [ -z "$(echo "$downloadsactive" | grep -Fxv "$downloadstotal")" ]; then
+  (cd /Media/Downloads/Complete || exit
+  echo "$downloadstodelete" | xargs rm)
 else
-  echo "There are no active downloads matched to total downloads. Something is wrong" | \
-  mail -s "The Plex download deletion script has failed" "$email"
+  echo "There are no active downloads matched to total downloads. Something is wrong." | \
+  mail -s "The Plex download deletion script has failed" \
+       -r "$from" \
+       "$email"
 fi
+
+unset IFS
