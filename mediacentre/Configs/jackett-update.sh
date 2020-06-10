22ee44173b9b317f4ebdeb823fe3ab30a7207246
Colin Matthews <colin@matthews-co.uk>
2020-06-10 10:38:50 +0100

    Create jackett-update.sh
    Updater for Jackett as it's built-in updater is broken in Linux

---
+++ mediacentre/Configs/jackett-update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+systemctl stop jacket
+sleep 5
+
+mv /opt/jackett /opt/jackettold
+
+( cd /tmp || return
+curl -s https://api.github.com/repos/Jackett/Jackett/releases | grep "browser_download_url".*Jackett.Binaries.LinuxAMDx64.tar.gz | head -1 | cut -d : -f 2,3 | tr -d \" | wget -i-
+tar -zxf Jackett.Binaries.LinuxAMDx64.tar.gz -C /opt/ )
+mv /opt/Jackett /opt/jackett
+chown -R jackett:jackett /opt/jackett
+
+rm -rf /opt/jackettold
+
+systemctl start jacket
