1e350197b9386e17f14f075eb78a8591c7e5b1e0
Colin Matthews <colin@Colin-MacBook-Pro.local>
2021-07-26 09:50:30 +0100

    Update CentOS8-baseline.sh

--- baseline/centos-rhel-amazon/CentOS8-baseline.sh
+++ baseline/centos-rhel-amazon/CentOS8-baseline.sh
@@ -18,7 +18,7 @@ timezone="Europe/London"
 # Packages needed
 yum install firewalld -y
 
-# Create 2GB swapfile
+# Create 4GB swapfile
 dd if=/dev/zero of=/mnt/swapfile bs=1M count=4096
 chown root:root /mnt/swapfile
 chmod 600 /mnt/swapfile
