c307960049c3fb737abcb3a52f15d82526af74e4
Colin Matthews <colin@Colin-MacBook-Pro.local>
2021-07-26 13:27:50 +0100

    Update permissions.sh
    Quoted directories to allow spaces

--- mediacentre/Configs/permissions.sh
+++ mediacentre/Configs/permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R sonarr:sonarr $tv
-chown -R radarr:radarr	$films
-chown -R transmission:transmission $downcomplete
+chown -R sonarr:sonarr "$tv"
+chown -R radarr:radarr	"$films"
+chown -R transmission:transmission "$downcomplete"
 chmod -R 0777 /Media
