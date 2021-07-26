cae50e11809ed3f77eda2cc641539461df64652a
Colin Matthews <colin@Colin-MacBook-Pro.local>
2021-07-26 12:04:50 +0100

    Update download-unrar.sh
    $PATH takes care of the absolute location

--- mediacentre/Configs/download-unrar.sh
+++ mediacentre/Configs/download-unrar.sh
@@ -3,5 +3,5 @@ torrentdir="$TR_TORRENT_DIR"/"$TR_TORRENT_NAME"
 cd "$torrentdir" || return
 for archive in ./*.rar
 do
-    /usr/local/bin/unrar e -r -o- "$archive"
+    unrar e -r -o- "$archive"
 done
