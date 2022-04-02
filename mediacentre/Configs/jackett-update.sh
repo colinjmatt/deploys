d9cc1c54ca3235a093b9d5f674a7ab7c8d0d6f3c
Colin Matthews <colinjmatt@users.noreply.github.com>
2022-04-02 21:48:00 +0100

    Update jackett-update.sh
    Removed email capabilities

--- mediacentre/Configs/jackett-update.sh
+++ mediacentre/Configs/jackett-update.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # Destination email and FROM address details for emailing any errors
-email="emailsed" # Email address of the recipient
-from="fromsed" # Email address of the sender
-fromname="fromnamesed" # Friendly name of the sender
-relaydomain="relaydomainsed" # Relay to send to
-
 x=""
 
 systemctl disable jackett --now
@@ -29,13 +24,4 @@ if [[ -d /opt/jackett ]]; then
   rm -rf /opt/jackettold
   rm /tmp/Jackett.Binaries.LinuxAMDx64.tar.gz
   systemctl enable jackett --now
-else
-  sendemail \
-    -f "$fromname <$from>" \
-    -t "$email" \
-    -u "Jackett update failed" \
-    -m "Jeckett update failed, reverting to previous version." \
-    -s "$relaydomain":25
-  mv /opt/jackettold /opt/jackett
-  exit 1
 fi
