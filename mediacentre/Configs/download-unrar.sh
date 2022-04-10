cfd24e7e4b01f4ce025b02c962cf0d8c594594ed
Colin Matthews <colinjmatt@users.noreply.github.com>
2022-04-10 17:41:07 +0100

    Update download-unrar.sh

--- mediacentre/Configs/download-unrar.sh
+++ mediacentre/Configs/download-unrar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-torrentdir="$TR_TORRENT_DIR"/"$TR_TORRENT_NAME"
-cd "$torrentdir" || exit 0
+torrentdir="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+
+cd "$torrentdir" || exit 1
 for archive in ./*.rar
 do
     unrar e -r -o- "$archive"
