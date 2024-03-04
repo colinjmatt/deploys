e2804add4c13a2b35b852011c5d95d33c1c0ac19
colinjmatt <colinjmatt@users.noreply.github.com>
2024-03-04 12:58:10 +0000

    Update ArchLinux-Zabbix-lxc.sh
    Overhauled the install script

--- zabbix-lxc/ArchLinux-Zabbix-lxc.sh
+++ zabbix-lxc/ArchLinux-Zabbix-lxc.sh
@@ -1,46 +1,115 @@
 #!/bin/bash
+# Zabbix install for Arch Linux based LXC container
 
-pacman -S --noconfirm zabbix-frontend-php zabbix-server zabbix-agent nginx
+pacman -Syu --noconfirm && pacman -S postgresql zabbix-server zabbix-frontend-php zabbix-agent fping nginx php-fpm php-gd php-pgsql sudo --noconfirm
+
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+sed -i -e "s/\#en_GB.UTF-8\ UTF-8/en_GB.UTF-8\ UTF-8/g" /etc/locale.gen
+locale-gen
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+echo "KEYMAP=uk" > /etc/vconsole.conf
+localectl set-keymap uk
+timedatectl set-ntp true
+
+hostnamectl set-hostname "zabbix"
+echo "zabbix" > /etc/hostname
+echo "127.0.0.1 localhost.localdomain localhost zabbix" > /etc/hosts
+
+echo -e "[Match]\nName=eth0\n\n[Address]\nAddress=192.168.1.131\n\n[Network]\nDNS=192.168.1.1\n\n[Route]\nGateway=192.168.1.1" >/etc/systemd/network/eth0.network
+
+sed -i -e "\
+  s/\;extension=bcmath/extension=bcmath/g; \
+  s/\;extension=gd/extension=gd/g; \
+  s/\;extension=gettext/extension=gettext/g; \
+  s/\;extension=ldap/extension=ldap/g; \
+  s/\;extension=mysqli/extension=pgsql/g; \
+  s/\;extension=sockets/extension=sockets/g; \
+  s/post_max_size\ =\ .*/post_max_size\ =\ 16M/g; \
+  s/max_execution_time\ =\ .*/max_execution_time\ =\ 300/g; \
+  s/max_input_time\ =\ .*/max_input_time\ =\ 300/g; \
+  s/;date.timezone\ =/date.timezone\ =\ \"Europe\/London\"/g" \
+/etc/php/php.ini
+
+su postgres -P -c 'initdb --locale=en_GB.UTF-8 -E UTF8 -D /var/lib/postgres/data'
+systemctl start postgresql
+su postgres -P -c 'psql -U postgres -c "create user zabbix"'
+su postgres -P -c 'psql -U postgres -c "create database zabbix --owner zabbix"'
+su postgres -P -c 'cat /usr/share/zabbix-server/postgresql/schema.sql | psql -d zabbix'
+su postgres -P -c 'cat /usr/share/zabbix-server/postgresql/images.sql | psql -d zabbix'
+su postgres -P -c 'cat /usr/share/zabbix-server/postgresql/data.sql | psql -d zabbix'
 
 ln -s /usr/share/webapps/zabbix /srv/http/zabbix
-cat ./Configs/ngnx.conf >/etc/nginx/nginx.conf
-
-createuser zabbix
-createdb zabbix -O zabbix
-cat /usr/share/zabbix-server/postgresql/{schema,images,data}.sql | psql -U zabbix -d zabbix
 
 sed -i -e "\
-  s/;extension=bcmath/extension=bcmath/g; \
-  s/;extension=curl/extension=curl/g; \
-  s/;extension=gd/extension=gd/g; \
-  s/;extension=gettext/extension=gettext/g; \
-  s/;extension=ldap/extension=ldap/g; \
-  s/;extension=pgsql/extension=pgsql/g; \
-  s/;extension=sockets/extension=sockets/g; \
-  s/;extension=zip/extension=zip/g; \
-  s/post_max_size\ =.*/post_max_size\ =\ 16M/g; \
-  s/max_execution_time \=.*/max_execution_time\ =\ 300/g; \
-  s/max_input_time\ =.*/max_input_time\ =\ 300/g; \
-  s/;date.timezone\ =.*/date.timezone\ =\ \"UTC\"/g" \
-/etc/php7/php.ini
-
-sed -i -e "\
-  s/DBName=.*/DBName=zabbix/g; \
-  s/DBUser=.*/DBUser=zabbix/g; \
-  s/DBPassword=.*/DBPassword=test/g; \
-  s/LogType=.*/LogType=system/g" \
+  s/\#\ DBPassword=/DBPassword=Zabb1x/g" \
 /etc/zabbix/zabbix_server.conf
 
-sed -i -e "\
-  s/Server=.*/Server=127.0.0.1/g; \
-  s/ServerActive=.*/ServerActive=127.0.0.1/g; \
-  s/Hostname=.*/Hostname=Zabbix\ server/g" \
-/etc/zabbix/zabbix_agent.conf
+echo 'Include=/etc/zabbix/zabbix_agentd.conf.d/*.conf' >>/etc/zabbix/zabbix_agentd.conf
+mkdir -p /etc/zabbix/zabbix_agentd.conf.d
+echo 'UserParameter=archlinuxupdates,checkupdates | wc -l' >/etc/zabbix/zabbix_agentd.conf.d/archlinuxupdates.conf
 
-openssl req -x509 -nodes -days 36500 -newkey rsa:4096 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
-openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
+cat <<EOT >/etc/nginx/nginx.conf
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    gzip on;
+    gzip_disable "msie6";
+
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen        80;
+        server_name   zabbix;
+        root /srv/http/zabbix;
+
+        location / {
+            index index.php;
+        }
+
+        location ~ \.php\$ {
+            try_files \$fastcgi_script_name =404;
+            include fastcgi_params;
+            fastcgi_pass			unix:/run/php-fpm/php-fpm.sock;
+            fastcgi_index			index.php;
+            fastcgi_buffers			8 16k;
+            fastcgi_buffer_size		32k;
+            fastcgi_param DOCUMENT_ROOT	\$realpath_root;
+            fastcgi_param SCRIPT_FILENAME	\$realpath_root\$fastcgi_script_name;
+        }
+    }
+}
+EOT
+
+openssl rand -hex 32 >/etc/zabbix/zabbix_agentd.psk
+chown zabbix-agent:zabbix-agent /etc/zabbix/zabbix_agentd.psk
+chmod 640 /etc/zabbix/zabbix_agentd.psk
+
+cat <<EOT >/etc/zabbix/zabbix_agentd.conf
+TLSConnect=psk
+TLSAccept=psk
+TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
+TLSPSKIdentity=PSK001
+EOT
 
 systemctl enable \
-  nginx \
+  postgresql \
+  zabbix-server-pgsql \
   zabbix-agent \
-  zabbix-server-pgsql --now
+  nginx \
+  php-fpm \
+  --now
