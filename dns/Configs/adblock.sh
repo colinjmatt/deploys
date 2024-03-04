b102c863cd050bc70f55ec9388d45859da1dca92
colinjmatt <colinjmatt@users.noreply.github.com>
2024-03-04 12:52:44 +0000

    Update adblock.sh
    Removed now-invalid URLs

--- dns/Configs/adblock.sh
+++ dns/Configs/adblock.sh
@@ -10,30 +10,29 @@ mkdir -p /tmp/adblock
 ( cd /tmp/adblock || return
 
 # Download and remove any CRLF line breaks from all lists
-wget -q -O- http://adaway.org/hosts.txt | tr -d "\r" >adaw-block.txt
-sed -i -e 1,24d adaw-block.txt
+if wget -q -O- http://adaway.org/hosts.txt | tr -d "\r" >adaw-block.txt; then
+  sed -i -e 1,24d adaw-block.txt
+fi
 
-wget -q -O- http://someonewhocares.org/hosts/zero/hosts | tr -d "\r" >danp-block.txt
-sed -i -e 1,85d danp-block.txt
+if wget -q -O- http://someonewhocares.org/hosts/zero/hosts | tr -d "\r" >danp-block.txt; then
+  sed -i -e 1,85d danp-block.txt
+fi
 
-wget -q -O- http://hosts-file.net/ad_servers.txt | tr -d "\r" >mphs-block.txt
-sed -i -e 1,10d mphs-block.txt
+if wget -q -O- http://winhelp2002.mvps.org/hosts.txt | tr -d "\r" >mvps-block.txt; then
+  sed -i -e 1,30d mvps-block.txt
+fi
 
-wget -q -O- http://winhelp2002.mvps.org/hosts.txt | tr -d "\r" >mvps-block.txt
-sed -i -e 1,30d mvps-block.txt
+if wget -q -O- https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt | tr -d "\r" >noco-block.txt; then
+  sed -i -e 1,11d noco-block.txt
+fi
 
-wget -q -O- http://www.malwaredomainlist.com/hostslist/hosts.txt | tr -d "\r" >mwdl-block.txt
-sed -i -e 1,5d mwdl-block.txt
-
-wget -q -O- https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt | tr -d "\r" >noco-block.txt
-sed -i -e 1,11d noco-block.txt
-
-wget -q -O- 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext' | tr -d "\r" >pgly-block.txt
-sed -i -e 1,14d pgly-block.txt
+if wget -q -O- 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext' | tr -d "\r" >pgly-block.txt; then
+  sed -i -e 1,14d pgly-block.txt
+fi
 
 # Remove hosts file IP entries
 sed -i -e " s/^0.0.0.0//
-            s/^127.0.0.1//" *
+            s/^127.0.0.1//" ./*
 
 # ALL FILES SANITATION
 # Use sed to perform the folloing (in order):
@@ -47,8 +46,8 @@ sed -i -e " s/^[ \t]*//
             s/\#.*$//
             /^\s*$/d
             s/^/address=\//
-            s/$/\/""$dns""/" *
-cat * | sort | sort -u >dnsmasq.adblock
+            s/$/\/""$dns""/" ./*
+cat ./* | sort | sort -u >dnsmasq.adblock
 
 mv dnsmasq.adblock /etc/ )
 
