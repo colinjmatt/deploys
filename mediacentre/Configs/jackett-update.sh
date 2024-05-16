cf49a7e3ee31ae6098c933e6d13f88d9253ebb7b
colinjmatt <colinjmatt@users.noreply.github.com>
2024-05-16 14:05:57 +0100

    Update jackett-update.sh
    Fully rewritten. Included a version check now

--- mediacentre/Configs/jackett-update.sh
+++ mediacentre/Configs/jackett-update.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
-x=""
 
-systemctl disable jackett --now
-sleep 5
+# Function to compare version numbers
+version_compare() {
+  if [[ $1 != "$2" ]]; then
+      stop_jackett
+      update_jackett "$2"
+      start_jackett
+  fi
+}
 
-mv /opt/jackett /opt/jackettold
+# Function to stop Jackett service
+stop_jackett() {
+  systemctl stop jackett
+}
 
-while [[ ! -d /opt/Jackett && $x -lt 5 ]]; do
-  ( cd /tmp || exit 1
-  curl -X GET https://api.github.com/repos/Jackett/Jackett/releases -H "Accept: application/json" | \
-  grep "browser_download_url.*Jackett.Binaries.LinuxAMDx64.tar.gz" | head -1 | cut -d : -f 2,3 | tr -d \" | wget -i-
-  tar -zxf Jackett.Binaries.LinuxAMDx64.tar.gz -C /opt/)
-  x=$(( x + 1 ))
-done
+# Function to start Jackett service
+start_jackett() {
+  systemctl start jackett
+}
 
-if [[ -d /opt/Jackett ]]; then
-  mv /opt/Jackett /opt/jackett
-  chown -R jackett:jackett /opt/jackett
-fi
+# Function to update Jackett
+update_jackett() {
+  # Define paths
+  install_dir="/opt/jackett"
+  temp_dir="/tmp/jackett_backup"
 
-if [[ -d /opt/jackett ]]; then
-  rm -rf /opt/jackettold
-  rm /tmp/Jackett.Binaries.LinuxAMDx64.tar.gz
-  systemctl enable jackett --now
-else
-  mv /opt/jackettold /opt/jackett
-  systemctl enable jackett --now
-  exit 1
-fi
+  # Backup the current installation
+  mv "$install_dir" "$temp_dir"
+
+  # Download the latest release from GitHub, extract it, and copy to /opt/
+  if curl -s -L "https://github.com/Jackett/Jackett/releases/download/$1/Jackett.Binaries.LinuxAMDx64.tar.gz" | tar -xz -C /opt/; then
+    # Rename installation directory to lowercase
+    mv "/opt/Jackett" "$install_dir"
+    # Change ownership recursively to jackett:jackett
+    chown -R jackett:jackett "$install_dir"
+  else
+    # Restore previous installation from backup
+    mv "$temp_dir" "$install_dir"
+    echo "Update failed. Restored previous installation."
+  fi
+}
+
+# Fetch the latest release version from GitHub API
+response=$(curl -s "https://api.github.com/repos/Jackett/Jackett/releases/latest")
+latest_version=$(echo "$response" | grep -o '"tag_name": ".*"' | cut -d'"' -f4)
+
+# Get the installed version of Jackett
+installed_version=$(/opt/jackett/jackett --version | awk '{print $2}')
+
+# Compare versions
+version_compare "$installed_version" "$latest_version"
