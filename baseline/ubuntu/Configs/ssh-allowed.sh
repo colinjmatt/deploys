b35b7b2902c0bd9edfd57c6f42dd4d32a9655ced
colinjmatt <colinjmatt@users.noreply.github.com>
2024-10-18 18:43:54 +0100

    Update ssh-allowed.sh
    Now checks if there's any rules that exist, and removes any/all old rules rather than just one.

--- baseline/ubuntu/Configs/ssh-allowed.sh
+++ baseline/ubuntu/Configs/ssh-allowed.sh
@@ -10,34 +10,48 @@ else
   lastip=""
 fi
 
-echo "$ip">/tmp/ipcheck
+echo "$ip" > /tmp/ipcheck
 
-# Only proceed if the IP has changed
+# Remove old rules if the IP has changed
 if [ "$ip" != "$lastip" ]; then
-  # Extract all rich rules under the drop zone
+  # Extract all rich rules under the drop zone matching the lastip
   firewallrules=$(firewall-cmd --zone=drop --list-all | sed -n '/rich rules:/,$p' | grep "rule family" | grep "$lastip")
-  
-  # Loop over each rule that matches the old IP and remove it
-  while IFS= read -r rule; do
-    firewall-cmd --zone=drop --remove-rich-rule "$rule"
-  done <<< "$firewallrules"
 
-  # Add the new rule with the updated IP
+  # Check if there are any rules to remove
+  if [ -n "$firewallrules" ]; then
+    # Loop over each rule that matches the old IP and remove it
+    while IFS= read -r rule; do
+      firewall-cmd --zone=drop --remove-rich-rule "$rule"
+    done <<< "$firewallrules"
+  fi
+fi
+
+# Function to check if a rich rule exists
+rule_exists() {
+  firewall-cmd --zone=drop --list-all | grep -q "rule family=\"ipv4\" source address=\"$ip\" port port=\"$1\" protocol=\"tcp\" accept"
+}
+
+# Add the new rules if they don't exist
+if ! rule_exists "22"; then
   firewall-cmd --zone=drop --add-rich-rule="
     rule family=\"ipv4\"
     source address=\"$ip\"
     port protocol=\"tcp\"
     port=\"22\"
     accept"
+fi
 
- firewall-cmd --zone=drop --add-rich-rule="
+if ! rule_exists "9091"; then
+  firewall-cmd --zone=drop --add-rich-rule="
     rule family=\"ipv4\"
     source address=\"$ip\"
     port protocol=\"tcp\"
     port=\"9091\"
     accept"
+fi
 
- firewall-cmd --zone=drop --add-rich-rule="
+if ! rule_exists "10050"; then
+  firewall-cmd --zone=drop --add-rich-rule="
     rule family=\"ipv4\"
     source address=\"$ip\"
     port protocol=\"tcp\"
