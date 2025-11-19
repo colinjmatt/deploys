a9189bbf6de6e7057f6049da4ca3c53d3feef9c8
colinjmatt <colinjmatt@users.noreply.github.com>
2025-11-19 19:35:27 +0000

    Update postgres-upgrade.sh
    General script improvements

--- zabbix-lxc/Configs/postgres-upgrade.sh
+++ zabbix-lxc/Configs/postgres-upgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euo pipefail
+
 lastversion="$(ls -l /var/cache/pacman/pkg/postgres*.tar.xz | grep -v libs | tail -2 | head -1 | awk -F ' ' '{print $NF}')"
 
-systemctl disable postgresql --now
+systemctl disable postgresql --now || true
 
 mv /var/lib/postgres/data /var/lib/postgres/olddata
 mkdir /var/lib/postgres/data /var/lib/postgres/tmp
@@ -15,7 +17,14 @@ tar xf postgres*.tar.xz
 su postgres -P -c 'initdb --locale=en_GB.UTF-8 -E UTF8 -D /var/lib/postgres/data'
 su postgres -P -c 'pg_upgrade -b /var/lib/postgres/tmp/usr/bin -B /usr/bin -d /var/lib/postgres/olddata -D /var/lib/postgres/data'
 systemctl enable postgresql --now
+su postgres -P -c 'reindexdb -d template1; reindexdb -d postgres; reindexdb -d zabbix'
+systemctl stop zabbix-server-pgsql || true
+su postgres -P -c 'psql -U postgres -c "\
+    ALTER DATABASE template1 REFRESH COLLATION VERSION; \
+    ALTER DATABASE postgres REFRESH COLLATION VERSION; \
+    ALTER DATABASE zabbix REFRESH COLLATION VERSION;"'
 su postgres -P -c '/usr/bin/vacuumdb --all --analyze-in-stages'
+systemctl start zabbix-server-pgsql
 )
 
 rm -rf /var/lib/postgres/tmp /var/lib/postgres/olddata
